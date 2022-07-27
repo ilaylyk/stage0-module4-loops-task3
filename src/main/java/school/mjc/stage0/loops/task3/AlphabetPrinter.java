247b1c2301bb475ad9e10f98f4c8827ec1e0ad69
ilaylyk <40176935+ilaylyk@users.noreply.github.com>
2022-07-27 04:02:35 +0300

    Update AlphabetPrinter.java

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
+        char[] chars = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        for (char aChar : chars ){
+            System.out.println(aChar);
+        }
     }
 }
