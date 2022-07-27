1408cce3f4fbdbb683cb62075ba49db9587e2b2d
ilaylyk <40176935+ilaylyk@users.noreply.github.com>
2022-07-27 04:03:55 +0300

    Update NinesSum.java

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,10 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
+        if (lengthOfLastNumber > 0) {
+            System.out.println((int) ((10 * (Math.pow(10, lengthOfLastNumber) - 1)) / 9 - lengthOfLastNumber));
+        } else {
+            System.out.println(0);
+        }
     }
 }
