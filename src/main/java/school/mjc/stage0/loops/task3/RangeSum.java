1c2ae5c5904479ecaee29623985dfadbebfe573a
ilaylyk <40176935+ilaylyk@users.noreply.github.com>
2022-07-27 04:04:16 +0300

    Update RangeSum.java

--- src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task3;
 
 public class RangeSum {
     public void printSumInclusive(int firstBoarder, int secondBoarder) {
+        long number = 0;
+        for (long i = firstBoarder; i <= secondBoarder; i++) {
+            number += i;
+        }
+        System.out.println(number);
     }
 }
